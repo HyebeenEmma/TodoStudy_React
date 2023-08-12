87175f4b28a846fe15b0014b7297314aadc836d8
bbae0312 <bbae7050@gmail.com>
2023-08-12 12:37:32 -0700

    Adding Login

--- src/Login.js
+++ src/Login.js
@@ -1,33 +1,37 @@
+import React, { useState } from "react";
+import { Link } from "react-router-dom"; // Import Link for navigation
 import "bootstrap/dist/css/bootstrap.css";
 import "bootstrap/dist/css/bootstrap.min.css";
 import "./App.css";
 
-
 const Login = () => {
-    const [id, setID] = useState();
-    const [password, setPassword] = useState();
+  const [id, setID] = useState("");
+  const [password, setPassword] = useState("");
 
-    return(
-        <FormWrapper
-        >
-        <InputWrapper>
-        ID
-        <input
-            type = "text"
-            autoFocus
-            placeholder = "아이디를 입력하세요"
-            onChange={(e) => setID(e.target.value)}
-        />
-        </InputWrapper>
-        <InputWrapper>
-        Password
-        <input
-            type = "password"
-            placeholder = "아이디를 입력하세요"
-            onChange={(e) => setID(e.target.value)}
-        />
-        </InputWrapper>
-        <Button type = "submit">로그인</Button>
-        </FormWrapper>
-    );
+  return (
+    <div className="login-form">
+      <label>ID</label>
+      <input
+        type="text"
+        autoFocus
+        placeholder="아이디를 입력하세요"
+        onChange={(e) => setID(e.target.value)}
+      />
+
+      <label>Password</label>
+      <input
+        type="password"
+        placeholder="비밀번호를 입력하세요"
+        onChange={(e) => setPassword(e.target.value)}
+      />
+
+      <button type="submit">로그인</button>
+
+      <p>
+        Don't have an account? <Link to="/signup">Sign up</Link>
+      </p>
+    </div>
+  );
 };
+
+export default Login;
--- src/index.js
+++ src/index.js
@@ -1,13 +1,20 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom";
+import { BrowserRouter, Route, Routes } from "react-router-dom"; // Import necessary components
+import "./index.css";
+import App from "./App";
+import Login from "./Login"; // Import the Login component
+import reportWebVitals from "./reportWebVitals";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <App />
+    <BrowserRouter>
+      <Routes>
+        <Route path="/login" element={<Login />} />
+        <Route path="/" element={<App />} />
+      </Routes>
+    </BrowserRouter>
   </React.StrictMode>
 );
 
